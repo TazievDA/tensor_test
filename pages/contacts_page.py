a23b9c49f05beb7b4b63709305ede90c2836a070
Denis Taziev <taziev.da@gmail.com>
2024-04-24 23:50:10 +0500

    Added second scenario of tests

--- pages/contacts_page.py
+++ pages/contacts_page.py
@@ -1,11 +1,15 @@
 from time import sleep
 
-from driver import Driver
+from driver import Driver, wait_element
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class ContactsPage:
     tensor_banner_xpath = '//a[contains(@class, "sbisru-Contacts__logo-tensor mb-12")]'
+    region_block_xpath = '//span[@class="sbis_ru-Region-Chooser__text sbis_ru-link"]'
+    partners_list_xpath = '//div[@class="controls-BaseControl controls_list_theme-sbisru controls_toggle_theme-sbisru"]'
+    concrete_region_xpath = '//span[@title="Камчатский край"]'
 
     @staticmethod
     def find_tensor_banner(driver: Driver):
@@ -17,3 +21,32 @@ class ContactsPage:
         tensor_banner = driver.find_element(By.XPATH, ContactsPage.tensor_banner_xpath)
         tensor_banner.click()
         driver.switch_to.window(driver.window_handles[-1])
+
+    @staticmethod
+    def find_region_block(driver: Driver):
+        region_block = driver.find_element(By.XPATH, ContactsPage.region_block_xpath)
+
+    @staticmethod
+    def find_partners_list(driver: Driver):
+        partners_list = driver.find_element(By.XPATH, ContactsPage.partners_list_xpath)
+        return partners_list
+
+    @staticmethod
+    def click_region_block(driver: Driver):
+        region_block = driver.find_element(By.XPATH, ContactsPage.region_block_xpath)
+        region_block.click()
+
+    @staticmethod
+    def click_concrete_region(driver: Driver):
+        concrete_region = driver.find_element(By.XPATH, ContactsPage.concrete_region_xpath)
+        ActionChains(driver).move_to_element(concrete_region).click(concrete_region).perform()
+
+    @staticmethod
+    def get_region_info(driver: Driver):
+        region_block = driver.find_element(By.XPATH, ContactsPage.region_block_xpath)
+        region_name = region_block.text
+        actual_url = driver.current_url
+        title = driver.title
+        return region_name, actual_url, title
+
+
--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from driver import Driver
 from pages.main_page import MainPage
 from pages.contacts_page import ContactsPage
@@ -6,16 +8,30 @@ from pages.tensor_about_page import TensorAboutPage
 
 def test_first_scenario(browser: Driver):
     MainPage.go_to_contacs(browser)
-    banner = ContactsPage.find_tensor_banner(browser)
-    assert banner == 'OK'
-    MainPage.go_to_contacs(browser)
+    ContactsPage.find_tensor_banner(browser)
     ContactsPage.click_tensor_banner(browser)
     TensorMainPage.tensor_find_text(browser)
     actual = TensorMainPage.tensor_click_more_button(browser)
-    assert actual == 'https://tensor.ru/about'
+    expected = 'https://tensor.ru/about'
+    assert actual == expected
     actual_set_images_size = TensorAboutPage.working_image_sizes(browser)
     expected = 1
     assert len(actual_set_images_size) == expected
 
 def test_second_scenario(browser: Driver):
     MainPage.go_to_contacs(browser)
+    ContactsPage.find_region_block(browser)
+    ContactsPage.find_partners_list(browser)
+    starter_partners_list = ContactsPage.find_partners_list(browser).text
+    ContactsPage.click_region_block(browser)
+    ContactsPage.click_concrete_region(browser)
+    sleep(1)
+    actual_region_name, actual_url, actual_title =  ContactsPage.get_region_info(browser)
+    changed_partners_list = ContactsPage.find_partners_list(browser).text
+    expected_region_name = 'Камчатский край'
+    expected_text_in_url = '41-kamchatskij-kraj'
+    expected_text_in_title = 'Камчатский край'
+    assert actual_region_name == expected_region_name
+    assert changed_partners_list != starter_partners_list
+    assert expected_text_in_url in actual_url
+    assert expected_text_in_title in actual_title
